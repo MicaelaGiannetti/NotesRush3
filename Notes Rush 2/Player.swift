8c8cd7b458ff70223b5b76c2234baf8d6f66e256
MicaelaGiannetti <Micaelagiannetti94@gmail.com>
2023-12-14 12:02:17 +0100

    JumpAction

--- Notes Rush 2/Player.swift
+++ Notes Rush 2/Player.swift
@@ -12,14 +12,20 @@ class Player : SKSpriteNode{
     var initialSize : CGSize = CGSize(width: 48, height: 56)
     var textureAtlas : SKTextureAtlas = SKTextureAtlas(named: "RunningPlayer")
     var runAnimation = SKAction()
-    
+    var jumpAnimation = SKAction()
+    var jumping = false
+    let maxJumpingForce : CGFloat = 57000
+    let maxHeight: CGFloat = 1000
  
     
     init(){
         super.init(texture:nil, color:.clear, size:initialSize)
         createAnimations()
-        self.run(runAnimation)
-        self.physicsBody = SKPhysicsBody(circleOfRadius:size.width/2)
+        self.run(runAnimation, withKey: "runningAnimation")
+        let bodyTexture = textureAtlas.textureNamed("player1")
+        self.physicsBody = SKPhysicsBody(texture: bodyTexture, size: self.size)
+        self.physicsBody?.mass = 30
+        self.physicsBody?.allowsRotation = false
         
     }
     func createAnimations() {
@@ -28,15 +34,26 @@ class Player : SKSpriteNode{
          textureAtlas.textureNamed("player2")]
         let runAction = SKAction.animate(with: runFrames, timePerFrame: 0.14)
         runAnimation = SKAction.repeatForever(runAction)
+        let jumpFrames: [SKTexture] =
+        [textureAtlas.textureNamed("player-jump")]
+        let jumpAction = SKAction.animate(with: jumpFrames, timePerFrame: 1)
+        jumpAnimation = SKAction.repeatForever(jumpAction)
+        
+        
     }
     
     func RunningPlayer () {
-        let moveRight = SKAction.moveBy(x:size.width, y:0, duration: 2)
+        let moveRight = SKAction.moveBy(x:size.width, y:0, duration: 0.5)
         let neverEndingRun = SKAction.repeatForever(moveRight)
         self.run(neverEndingRun)
+    }
+    
+    func update(){
+        self.physicsBody?.applyForce(CGVector (dx:0, dy:500))
     }
     
     required init? (coder aDecoder: NSCoder){
         super.init(coder:aDecoder)
     }
 }
+
